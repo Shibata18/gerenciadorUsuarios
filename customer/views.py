ec3275688e2fb9d0c1bab3e667e514c79dca55cd
Shibata18 <douglas.shibata11@gmail.com>
2020-08-23 13:47:48 -0300

    CRUD e paginação

--- customer/views.py
+++ customer/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from .models import Customer
 from .forms import CustomerForm
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 # Create your views here.
 class CustomerListView(ListView):
     template_name = 'customer/customer_list.html'
+    paginate_by = 2
     model = Customer
     queryset = Customer.objects.all()
 
@@ -15,5 +17,28 @@ class CustomerCreateView(CreateView):
 
     def form_valid(self,form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+
+class CustomerUpdateView(UpdateView):
+    template_name = 'customer/customer.html'
+    form_class = CustomerForm
+
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Customer,id=id)
+
+    def form_valid(self,form):
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+class CustomerDeleteView(DeleteView):
+ 
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Customer,id=id)
+
     def get_success_url(self):
         return reverse('customer:customer-list')
