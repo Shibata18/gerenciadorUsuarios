d3ffcfcb8b2a7e477a4c317e027c6b7408d2fa86
Shibata18 <douglas.shibata11@gmail.com>
2020-08-23 15:09:31 -0300

    Desenvolvimento

--- crm/settings.py
+++ crm/settings.py
@@ -78,12 +78,15 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-""" DATABASES = {
+""" 
+#desenolvimento
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 } """
+#Produção
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
