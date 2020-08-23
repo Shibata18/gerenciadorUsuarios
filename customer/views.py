1f38eb989a937e9e918c971aace0d9fd80c79108
Shibata18 <douglas.shibata11@gmail.com>
2020-08-23 13:57:35 -0300

    Busca implementada

--- customer/views.py
+++ customer/views.py
@@ -4,12 +4,20 @@ from .models import Customer
 from .forms import CustomerForm
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 # Create your views here.
 class CustomerListView(ListView):
     template_name = 'customer/customer_list.html'
     paginate_by = 2
     model = Customer
-    queryset = Customer.objects.all()
+    def get_queryset(self):
+        name = self.request.GET.get('name')
+        if name:
+            object_list = self.model.objects.filter(Q(first_name__icontains=name)|Q(last_name__icontains=name))
+        else:
+            object_list = self.model.objects.all()
+        return object_list
+    #queryset = Customer.objects.all()
 
 class CustomerCreateView(CreateView):
     template_name = 'customer/customer.html'
