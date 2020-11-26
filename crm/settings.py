8133869c2d6d8fc762eb10abad1cd15f78517af0
Douglas Shibata <douglas.shibata11@gmail.com>
2020-11-26 11:15:04 -0300

    Mudando configurações para o deploy

--- crm/settings.py
+++ crm/settings.py
@@ -78,17 +78,17 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-""" 
+ 
 #desenolvimento
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+}
 #Produção
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
